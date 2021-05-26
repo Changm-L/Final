d97ff448c5edfd40cb4c30cec2c3055de4b55fdc
chldydgml <84460998+chldydgml@users.noreply.github.com>
2021-05-26 11:46:42 +0900

    시설 상세 페이지 틀

--- app/src/main/java/com/example/afinal/MainActivity.java
+++ app/src/main/java/com/example/afinal/MainActivity.java
@@ -1,14 +1,62 @@
 package com.example.afinal;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
+import android.widget.TableLayout;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends FragmentActivity {
+
+    TabLayout tabs;
+
+    HomeFragment fragment_home;
+    FeastFragment fragment_feast;
+    ReviewFragment fragment_review;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragment_home = new HomeFragment();
+        fragment_feast = new FeastFragment();
+        fragment_review = new ReviewFragment();
+
+        getSupportFragmentManager().beginTransaction().add(R.id.container, fragment_home).commit();
+
+        tabs = findViewById(R.id.tabs);
+        tabs.addTab(tabs.newTab().setText("홈"));
+        tabs.addTab(tabs.newTab().setText("공연•행사"));
+        tabs.addTab(tabs.newTab().setText("리뷰"));
+
+        tabs.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                int position = tab.getPosition();
+                Fragment selected = null;
+                if(position == 0)
+                    selected = fragment_home;
+                else if(position == 1)
+                    selected = fragment_feast;
+                else if(position == 2)
+                    selected = fragment_review;
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, selected).commit();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 }
