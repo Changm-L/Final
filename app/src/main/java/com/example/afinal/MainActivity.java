8aa229251e552747192fc7430e2f8420f223050e
Changm-L <chang6585@naver.com>
2021-06-03 16:13:51 +0900

    API 지도 검색 기본 틀 완성

--- app/src/main/java/com/example/afinal/MainActivity.java
+++ app/src/main/java/com/example/afinal/MainActivity.java
@@ -1,46 +1,79 @@
 package com.example.afinal;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
-public class MainActivity extends AppCompatActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.Toast;
 
-    ImageView imgv1;
-    ImageView imgv2;
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends FragmentActivity {
+
+    TabLayout tabs;
+
+    HomeFragment fragment_home;
+    FeastFragment fragment_feast;
+    ReviewFragment fragment_review;
+
     private DrawerLayout drawerLayout;
     private View drawerView;
+    Button go;
+    EditText id, pw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imgv1 = findViewById(R.id.imageView9);
-        imgv2 = findViewById(R.id.imageView6);
-        imgv1.setOnClickListener(new View.OnClickListener() {
+        fragment_home = new HomeFragment();
+        fragment_feast = new FeastFragment();
+        fragment_review = new ReviewFragment();
+
+        getSupportFragmentManager().beginTransaction().add(R.id.container, fragment_home).commit();
+
+        tabs = findViewById(R.id.tabs);
+        tabs.addTab(tabs.newTab().setText("홈"));
+        tabs.addTab(tabs.newTab().setText("공연•행사"));
+        tabs.addTab(tabs.newTab().setText("리뷰"));
+
+        tabs.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), SubActivity.class);
-                startActivity(intent);
+            public void onTabSelected(TabLayout.Tab tab) {
+                int position = tab.getPosition();
+                Fragment selected = null;
+                if(position == 0)
+                    selected = fragment_home;
+                else if(position == 1)
+                    selected = fragment_feast;
+                else if(position == 2)
+                    selected = fragment_review;
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, selected).commit();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
             }
         });
 
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawerView = (View) findViewById(R.id.drawerView);
         drawerLayout.setDrawerListener(listener);
-
-        imgv2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                drawerLayout.openDrawer(drawerView);
-            }
-        });
 
 
     }
